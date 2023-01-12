16fe55e5d1739dfca442854de636c72dd323c544
James <jamesang@ualberta.ca>
2023-01-11 22:13:37 -0700

    finished 3 exercise files for chapter 10

---
+++ PythonCrashCourse/chapter10/10.1 - learning_python.py
@@ -0,0 +1,19 @@
+'''Open a blank file in your text editor and write a few lines
+summarizing what you’ve learned about Python so far. Start each line with the
+phrase In Python you can. . . . Save the file as learning_python.txt in the same
+directory as your exercises from this chapter. Write a program that reads the file
+and prints what you wrote two times: print the contents once by reading in the
+entire file, and once by storing the lines in a list and then looping over each line'''
+
+
+
+from pathlib import Path
+
+path = Path('chapter10/learning_python.txt')
+content = path.read_text().rstrip()
+
+# content_list = content.splitlines()
+
+for line in content.splitlines():
+    print(line)
+
